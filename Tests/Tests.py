a3de8673a1873a02f94bcc7ae108c11554198437
SkyLined <github@skylined.nl>
2021-10-01 14:16:09 +0200

    Update Tests.py

--- Tests/Tests.py
+++ Tests/Tests.py
@@ -1,2 +1,9 @@
+import os, sys;
+sModulePath = os.path.dirname(__file__);
+sys.path = [sModulePath] + [sPath for sPath in sys.path if sPath.lower() != sModulePath.lower()];
+
 from fTestDependencies import fTestDependencies;
 fTestDependencies();
+
+# I should add some tests here.
+pass;
